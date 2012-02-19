874265231abe3820ecf846a68e070f17710b8e96
sorenbs <sorenbs@gmail.com>
2012-02-19 23:31:21 +0100

    added player and hittest

--- bbg3/game.js
+++ bbg3/game.js
@@ -1,11 +1,40 @@
-window.onload = function() {
+window.onload = function () {
 	//start crafty
 	Crafty.init(960, 640);
 	Crafty.canvas.init();
-	
+
 
 	Crafty.modules({ TiledLevel: 'dev-uncompressed' }, function () {
 		//module is ready
-		Crafty.e("TiledLevel").tiledLevel("map.json");
+
+		Crafty.c("Solid", {
+			init: function () {
+				this.requires("Color").color("red");
+			}
+		});
+
+		Crafty.scene("Game", function () {
+			Crafty.e("TiledLevel").tiledLevel("map.json");
+
+			var player = Crafty.e("Player, Color, 2D, DOM, Collision")
+				.color("green")
+				.attr({ x: 32, y: 32, w: 28, h: 28 })
+				.bind('Moved', function (from) {
+				if (this.hit('Solid')) {
+					this.attr({ x: from.x, y: from.y });
+				}
+			});
+
+			Crafty.modules({ MoveTo: "release" }, function () {
+				player.requires("MoveTo");
+			});
+
+
+
+
+
+		});
+
+		Crafty.scene("Game");
 	});
 };
